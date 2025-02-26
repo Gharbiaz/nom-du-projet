7a9a3afa16b962ef79b269f2d576018ab7c68af9
MSI <Aziz.belhiba@sesame.com.tn>
2025-02-26 13:15:17 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import ordi from './ordinateur.jpg'
 function App() {
+  const computer = {
+    name: "Ordinateur Portable MSI",
+    imageUrl: ordi, 
+    description: "Un ordinateur portable performant avec un processeur puissant, un grand stockage et un écran haute définition. Idéal pour les professionnels et les gamers.",
+    price: "1299DT"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <div className="row">
+        <div className="col-md-6">
+          {/* Image */}
+          <img src={computer.imageUrl} alt={computer.name} className="img-fluid rounded shadow-lg" />
+        </div>
+        <div className="col-md-6">
+          {/* Computer Details */}
+          <h1>{computer.name}</h1>
+          <p>{computer.description}</p>
+          <h3 className="text-success">{computer.price}</h3>
+          <button className="btn btn-primary btn-lg mt-3">Acheter Maintenant</button>
+        </div>
+      </div>
     </div>
   );
 }
